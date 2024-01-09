5c35a2106dcfab4a1a146a17e4edbd1191966d4b
Keittiokyynikko <karvinen.juuso@gmail.com>
2024-01-09 16:19:05 +0200

    toka

---
+++ storyblok/Modal.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-editable="blok">
+        <StoryBlokComponent 
+            v-for="blok in blok.columns"
+            :key="blok._uid"
+            :blok="blok"
+        />
+    </div>
+</template>
+
+<script setup>
+    defineProps({ blok: Object})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
