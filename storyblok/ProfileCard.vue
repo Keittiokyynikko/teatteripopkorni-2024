4ecdd37e4cf2e6789ec43d10dddaa568ffc5550e
Juuso Karvinen <juusokarvinen@Juusos-MBP.home>
2024-03-09 10:24:15 +0200

    profiilikuvat

--- storyblok/ProfileCard.vue
+++ storyblok/ProfileCard.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="profile-card m-3 col d-flex flex-column justify-content-start align-items-center" v-editable="blok">
-            <img class="profile mb-5" :src="blok.ProfileImage.filename">
+            <div class="profile mb-5">
+                <img class="profile-image" :src="blok.ProfileImage.filename">
+            </div>
             <h5 class="text-center">{{ blok.Name }}</h5>
             <p class="text-center">{{ blok.Description }}</p>
     </div>
@@ -13,10 +15,21 @@
 <style lang="scss" scoped>
 
     .profile {
+        aspect-ratio: 1/1;
+        position: relative;
         min-width: 200px;
         min-height: 200px;
-        border-radius: 100px;
+        max-width: 200px;
+        max-height: 200px;
+        border-radius: 100%;
         background-color: white;
+        overflow: hidden;
+        
+    }
+
+    .profile-image {
+        width: 100%;
+        height: auto;
     }
 
 </style>
