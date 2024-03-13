ac38944ac923cde8435acb58100e2eafab97b5df
Juuso Karvinen <juusokarvinen@Juusos-MBP.home>
2024-03-13 11:04:38 +0200

    token-testi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,7 +46,7 @@ export default defineNuxtConfig({
     },
 },
   devtools: { enabled: true },
-  modules: [['@storyblok/nuxt', { accessToken: process.env.ACCESS_TOKEN }]],
+  modules: [['@storyblok/nuxt', { accessToken: process.env.ACCESS_TOKEN_P }]],
   css: [
     '@/assets/styles/main.scss',
     'bootstrap/dist/css/bootstrap.css'
